d6c770cbff2b766223ea863112c0f2a07153a2f5
tianshilu <tianshi.lu@utsouthwestern.edu>
2017-11-06 13:38:18 -0600

    part2

--- lab1/part2/part2_3.py
+++ lab1/part2/part2_3.py
@@ -3,7 +3,7 @@ part1=[]
 for i in f:
     i=i.split('\t')
     i[3]=i[3].replace('\r\n','')
-    if i[3]!='Direction'and int(i[3])<0:
+    if int(i[3])<0:
         a=i[0]
         i[0]=i[1]
         i[1]=a
@@ -19,20 +19,53 @@ for j in part1:
 for m in range(len(part1)):
     o=part1[m][0]
     b=part1[m][1]
-    for n in range(m+1,len(part1)):
-        if part1[n][1]==o:
+    mdir=part1[m][2]
+    n=m+1
+    while n<len(part1):
+        left,right='',''
+        if part1[n][1]==o and part1[n][2]=='F':
             a=part1[n][0]
-            if [a,b] in edge:
+            if [a,b] in edge and [a,b] not in redu:
                 redu.append([a,b])
-        elif part1[n][0]==b:
+
+#                if a=='001' or b=='001':
+#                   print 'left',[a,b]
+#                    print part1[n],part1[m]
+            else:
+                left=part1[n][0]
+            n+=1
+        elif part1[n][0]==b and mdir=='F':
             c=part1[n][1]
-            if [o,c] in edge:
+            if [o,c] in edge and [o,c] not in redu:
                 redu.append([o,c])
+             #   if o=='001' or c=='001':
+              #      print 'right',[o,c]
+               #     print part1[m],part1[n]
+            else:
+                right=part1[n][1]
+            n+=1
+        else:
+            n+=1
+        if n==len(part1) and left!='':
+            o=left
+            n=m+1
+        if n==len(part1) and right!='':
+            b=right
+            n=m+1
 for q in redu:
     for p in part1:
         if q==[p[0],p[1]]:
             part1.remove(p)
-            
+#debug
+'''
+print part1,len(part1)
+eg=[]
+for e in part1:
+    eg.append(e[0])
+    eg.append(e[1])
+print sorted(eg)
+#debug
+'''
 num=len(part1)
 for b in range(0,num):
     part1[b].insert(-2,'F')
@@ -81,7 +114,7 @@ while part1!=[]:
                 for h in part1:
                     if h[0]==nex[1] and h[1]==nex[0]:
                         part1.remove(h)
-                    elif h[1]==nex[1] and h[3]==nex[3]:
+                    elif h[1]==nex[1] and h[3]==nex[3] and nex in uni:
                         uni.pop(-1)
                         part1.remove(h)
                         q=len(part1)
@@ -96,7 +129,7 @@ while part1!=[]:
                 for g in part1:
                     if g[0]==bef[1] and g[1]==bef[0]:
                         part1.remove(g)
-                    elif g[0]==bef[0] and g[2]==bef[2]:
+                    elif g[0]==bef[0] and g[2]==bef[2] and nex in uni:
                         part1.remove(g)
                         for k in part1:
                             if k[0]==g[1] and k[1]==g[0]:
@@ -115,8 +148,7 @@ for item in uni_list:
     num+=1
     for f in item:
         len_uni+=int(f[4])
-    tlist.append('%s\t%02d\t%i\t%i'%('UNI',num,len(item)+1,len_uni+250))
-    tlist.append('%s\t%s\t%s'%('ReadID','Direction','Offset'))
+    tlist.append('%s\t%02d\t%i\t%i'%('UNI',num,len(item)+1,len_uni+500))
     for e in item:
         if e==item[0]:
             tlist.append('%s\t%s\t%i'%(e[0],e[2],0))
@@ -124,7 +156,6 @@ for item in uni_list:
         else:
             tlist.append('%s\t%s\t%i'%(e[1],e[3],int(e[4])))
     
-print tlist
 result_file=file('/home2/s171162/work/compbio/lab1/part2/sample_uni.txt','w')
 
 for o in tlist:
