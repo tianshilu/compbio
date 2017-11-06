1bbbfa9ea73c14907683e9412ce79b65b77a120c
tianshilu <tianshi.lu@utsouthwestern.edu>
2017-11-06 16:12:15 -0600

    part2

--- lab1/part2/part2_3.py
+++ lab1/part2/part2_3.py
@@ -1,4 +1,4 @@
-f=file('/home2/s171162/work/compbio/lab1/sample_data/sample_KMP.txt')
+f=file('/home2/s171162/work/compbio/lab1/part1_KMP.txt')
 part1=[]
 for i in f:
     i=i.split('\t')
@@ -86,7 +86,7 @@ while part1!=[]:
             part1.remove(u)
     part1.pop(0)
     m,n,q=0,0,0
-    nex,bef=0,0
+    nex,bef='',''
     while q<len(part1):
         if b==part1[q][0] and bd==part1[q][2]:
             if part1[q]!=nex:
@@ -116,8 +116,26 @@ while part1!=[]:
                         part1.remove(h)
                     elif h[1]==nex[1] and h[3]==nex[3] and nex in uni:
                         uni.pop(-1)
+                        b='rightend'
+                        bd='rightenddir'
                         part1.remove(h)
-                        q=len(part1)
+                        mulr,mul=0,[]
+                        for k in part1:
+                            if k[0]==h[1] and k[1]==h[0]:
+                                part1.remove(k)
+                            elif k[0]==nex[1] and k[2]==nex[3]:
+                                mulr+=1
+                                mul.append(k)
+                        if mulr>1:
+                            uni_list.append('%s\t%s\t%i'%(nex[1],nex[3],0))
+                            for l in mul:
+                                part1.remove(l)
+                                for r in part1:
+                                    if l[0]==r[1] and l[1]==r[0]:
+                                        part1.remove(r)
+                        elif mulr==0:
+                            uni_list.append('%s\t%s\t%i'%(nex[1],nex[3],0))
+                                                    
             elif m>1:
                 q+=1
             if n==1:
@@ -130,13 +148,26 @@ while part1!=[]:
                     if g[0]==bef[1] and g[1]==bef[0]:
                         part1.remove(g)
                     elif g[0]==bef[0] and g[2]==bef[2] and nex in uni:
-                        part1.remove(g)
-                        for k in part1:
-                            if k[0]==g[1] and k[1]==g[0]:
-                                part1.remove(k)
                         uni.pop(0)
-                        a=uni[0][0]
-                        ad=uni[0][2]
+                        a='leftend'
+                        ad='leftenddir'
+                        part1.remove(g)
+                        mul,mu=0,[]
+                        for s in part1:
+                            if s[0]==g[1] and s[1]==g[0]:
+                                                part1.remove(s)
+                            elif s[1]==bef[0] and s[3]==bef[2]:
+                                                mul+=1
+                                                mu.append(s)
+                        if mul>1:
+                            uni_list.append('%s\t%s\t%i'%(bef[0],bef[2],0))
+                            for t in mul:
+                                part1.remove(t)
+                                for v in part1:
+                                    if t[0]==v[1] and t[1]==v[0]:
+                                        part1.remove(v)
+                        elif mul==0:
+                            uni_list.append('%s\t%s\t%i'%(bef[0],bef[2],0))
             elif n>1:
                 q+=1
     uni_list.append(uni)
@@ -146,17 +177,21 @@ num=0
 for item in uni_list:
     len_uni=0
     num+=1
-    for f in item:
-        len_uni+=int(f[4])
-    tlist.append('%s\t%02d\t%i\t%i'%('UNI',num,len(item)+1,len_uni+500))
-    for e in item:
-        if e==item[0]:
-            tlist.append('%s\t%s\t%i'%(e[0],e[2],0))
-            tlist.append('%s\t%s\t%i'%(e[1],e[3],int(e[4])))
-        else:
-            tlist.append('%s\t%s\t%i'%(e[1],e[3],int(e[4])))
-    
-result_file=file('/home2/s171162/work/compbio/lab1/part2/sample_uni.txt','w')
+    if isinstance(item,str):
+        print item
+        tlist.append('%s\t%02d\t%i\t%i'%('UNI',num,1,500))
+        tlist.append(item)
+    else:
+        for f in item:
+            len_uni+=int(f[4])
+        tlist.append('%s\t%02d\t%i\t%i'%('UNI',num,len(item)+1,len_uni+500))
+        for e in item:
+            if e==item[0]:
+                tlist.append('%s\t%s\t%i'%(e[0],e[2],0))
+                tlist.append('%s\t%s\t%i'%(e[1],e[3],int(e[4])))
+            else:
+                tlist.append('%s\t%s\t%i'%(e[1],e[3],int(e[4])))    
+result_file=file('/home2/s171162/work/compbio/lab1/part2_uni.txt','w')
 
 for o in tlist:
     result_file.write('%s\n'%o)
